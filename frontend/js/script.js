81cb2d5e4bf83a8ca5ff5d9132276ddbf3246029
Carlosxc-dev <chcruz64@gmail.com>
2024-09-10 14:54:03 -0300

    up

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,7 +3,7 @@ const addForm = document.querySelector(".add-form");
 const inputTask = document.querySelector(".input-task");
 const number_html = document.querySelector(".n_task");
 
-const url = "https://project-to-do-list-h4ox.vercel.app/";
+const url = "https://project-to-do-list-h4ox.vercel.app";
 
 //api node que faz requisicao para backend
 const fetchTasks = async () => {
